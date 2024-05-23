bd3d947d3897a1c56d862237ecb52524af9738b4
bianca23ioana <161381305+bianca23ioana@users.noreply.github.com>
2024-05-23 15:40:09 +0300

    am adaugat niste chestii.. nu au sens momentan

---
+++ src/app/Models/search.ts
@@ -0,0 +1,9 @@
+export class Search{
+    startDate: number;
+    endDate: number;
+    constructor(startDate:number, endDate:number){
+        this.startDate=startDate;
+        this.endDate=endDate;
+    }
+
+}
---
+++ src/app/pages/available/available.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-available',
+  templateUrl: './available.component.html',
+  styleUrl: './available.component.css'
+})
+export class AvailableComponent {
+
+}
